e053b7ca748926e5f649c40ffdb41685a913ae69
Luis Urraca <lurraca@pivotal.io>
2019-04-30 19:25:46 +0100

    Display error message if Project Id env vars is not set
    Closes #1

--- main.go
+++ main.go
@@ -8,7 +8,11 @@ import (
 
 func main() {
 	trackerApiKey := os.Getenv("TRACKER_API_KEY")
+	trackerProjectID := os.Getenv("TRACKER_PROJECT_ID")
 	if trackerApiKey == "" {
 		log.Fatal(fmt.Sprintf("missing TRACKER_API_KEY environment variable"))
+	}
+	if trackerProjectID == "" {
+		log.Fatal(fmt.Sprintf("missing TRACKER_PROJECT_ID environment variable"))
 	}
 }
--- main_test.go
+++ main_test.go
@@ -12,22 +12,60 @@ import (
 var _ = Describe("Storymate CLI tool", func() {
 	var (
 		session *gexec.Session
+		envs    *envVars
 	)
 
 	JustBeforeEach(func() {
 		cmd := exec.Command(storymateBinary)
-		// cmd.Env = envs.toStringArray()
+		cmd.Env = envs.toStringArray()
 
 		var err error
 		session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 	})
 
+	BeforeEach(func() {
+
+		envs = &envVars{trackerApiKey: "TEST_API_KEY", trackerProjectID: "TEST_PROJECT_ID"}
+	})
 
 	Context("when the app is not configured correctly", func() {
-		It("logs that the API KEY is not set", func() {
+		Context("Tracker API Key", func(){
+		BeforeEach(func() {
+			envs.trackerApiKey = ""
+		})
+
+		It("logs that the API Key is not set", func() {
 			Eventually(session.Err).Should(Say("missing TRACKER_API_KEY environment variable"))
+		})
+		})
+
+		Context("Project ID", func() {
+			BeforeEach(func(){
+				envs.trackerProjectID = ""
+			})
+
+			It("logs that the Project ID is not set", func() {
+				Eventually(session.Err).Should(Say("missing TRACKER_PROJECT_ID environment variable"))
+			})
 		})
 	})
 })
 
+type envVars struct {
+	trackerApiKey      string
+	trackerProjectID   string
+}
+
+func (e *envVars) toStringArray() []string {
+	result := []string{}
+
+	if e.trackerApiKey != "" {
+		result = append(result, "TRACKER_API_KEY="+e.trackerApiKey)
+	}
+	if e.trackerProjectID != "" {
+		result = append(result, "TRACKER_PROJECT_ID="+e.trackerProjectID)
+	}
+
+	return result
+}
